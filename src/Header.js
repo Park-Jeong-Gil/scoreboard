958c2b7fc2dc13580c79b6705f4d57fe8e2a05da
brm <magnet0706@naver.com>
2019-09-28 17:48:50 +0900

    Stats 컴포넌트 생성

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Stats} from './Stats';
 
 // function component 만드는 법
 // 1. 첫글자가 대문자
@@ -7,8 +8,8 @@ export const Header = (props) => {
   console.log(props);
   return (
     <header className="header">
+      <Stats/>
       <h1 className="h1">{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
     </header>
   );
 }
