4f43e5e18ebd8f2550efd775b8da4684bf174101
전찬근 <pocksrms@naver.com>
2019-10-05 11:50:52 +0900

    Stopwatch - 타이머 실행, 리셋 로직 구현

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
 export class Stopwatch extends React.Component {
-  tickRef
+  tickRef;
+
+  state ={
+    isRunning : false,
+    timer : 0
+  }
+
+  handleStopwatch=()=>{
+    this.setState(prevState => ({isRunning: !prevState.isRunning}));
+  }
 
   render() {
     return (
       <div className='stopwatch'>
         <h2>Stop Watch</h2>
-        <span className='stopwatch-time'>0</span>
-        <button>Start</button>
-        <button>Reset</button>
+        <span className='stopwatch-time'>{this.state.timer}</span>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop':'Start'}</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
 
-  tick(){}
+  tick = () => {
+    if (this.state.isRunning){
+      this.setState(prevState => ({timer:prevState.timer+1}));
+    }
+  }
+
+  handleReset = () =>{
+    this.setState({timer:0});
+  }
+
   // DOM이 렌더링 된 직후에 호출되는 라이프 사이클
   // 3rd 라이브러리 로딩, 네트워크 호출
   componentDidMount() {
@@ -24,6 +42,6 @@ export class Stopwatch extends React.Component {
   // DOM이 파괴되기 직전에 호출되는 라이프 사이클
   // 리소스 해제 등등
   componentWillUnmount() {
-    clearInterval(this.tickRef)
+    clearInterval(this.tickRef);
   }
 }
