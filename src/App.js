74d47d542dadb4a0aff97f01abb86eac6a43bdf8
전찬근 <pocksrms@naver.com>
2019-10-12 16:18:25 +0900

    Score board 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from './Header';
 import Player from './Player';
 import AddPlayerFrom from "./AddPlayerFrom";
---
+++ src/pages/scoreboard/Scoreboard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './scoreboard.css';
+import Header from "../../Header";
+import {CustomPlayer} from "../../CustomPlayer";
+import AddPlayerFrom from "../../AddPlayerFrom";
+import {connect} from "react-redux";
+
+class Scoreboard extends React.Component {
+  getHighScore(){
+    let max = 0;
+    this.props.players.forEach( player =>{
+      if(player.score > max){
+        max = player.score;
+      }
+    });
+    return max;
+  }
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header />
+        {
+          this.props.players.map((player) => {
+            return (
+              <CustomPlayer name={player.name} score={player.score} id={player.id} key={player.id}
+                // 2. props로 콜백 펑션을 전달
+                            isHighScore={player.score == this.getHighScore()}
+              />
+            )
+          })
+        }
+        <AddPlayerFrom/>
+      </div>
+    );
+  }
+
+
+}
+
+const mapStateToProps = (state) => ({
+  // 왼쪽은 props, 오른쪽이 store state
+  players: state.playerReducer.players
+})
+
+export default connect(mapStateToProps)(Scoreboard);
